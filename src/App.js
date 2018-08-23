0fb9cb53e1cbc0d139e8fa503426466064b0ac5c
Mikaiyl <mikaiyl.davis@icloud.com>
2018-08-23 10:31:30 -0400

    Init redux/react-redux/redux-thunk

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux'
+
 import logo from './logo.svg';
 import './App.css';
 
@@ -18,4 +20,4 @@ class App extends Component {
   }
 }
 
-export default App;
+export default connect()(App);
