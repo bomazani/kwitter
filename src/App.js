2fe71beb0653f5186d5698a6f82be018c5141954
Mikaiyl <mikaiyl.davis@icloud.com>
2018-08-23 10:56:30 -0400

    oops

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import { connect } from 'react-redux'
+import { connect } from 'react-redux';
+import Message from './components/message.jsx';
 
 import logo from './logo.svg';
 import './App.css';
@@ -12,6 +13,7 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
+        <Message />
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
