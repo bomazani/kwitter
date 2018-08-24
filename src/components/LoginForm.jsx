53e39f8c35d06388d45f60b46235f7338d7e04dd
Bob Hunt <bob.hunt.email@gmail.com>
2018-08-24 11:00:43 -0400

    updated LoginForm. Added display name field.

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -1,17 +1,21 @@
 import React from 'react';
-import { Button } from 'semantic-ui-react';
-
-    
+import { Segment, Form, Input } from 'semantic-ui-react';
+  
 class LoginForm extends React.Component {
+
     render() {
-        return 
-            <React.fragment>
-                <p>login form</p>
-                <Button inverted color='blue'>Login</Button>
-            </React.fragment>
-        
-    
+        return (
+            <Segment raised>
+            <Form>
+                <Form.Field>
+                <Input placeholder='Username' />
+                <Input placeholder='password' type='password' />
+                <Input placeholder='Display Name' />                </Form.Field>
+            </Form>
+            </Segment>
+        )    
     }
 }
+
 
 export default LoginForm;
