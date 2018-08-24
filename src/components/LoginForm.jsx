cf4115b55ef9491fa863010637f1daa060d7af3a
Bob Hunt <bob.hunt.email@gmail.com>
2018-08-24 10:38:00 -0400

    modified src/components/LoginForm

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -1,11 +1,17 @@
 import React from 'react';
+import { Button } from 'semantic-ui-react';
+
+    
 class LoginForm extends React.Component {
     render() {
-        return <p>login form</p>
-
+        return 
+            <React.fragment>
+                <p>login form</p>
+                <Button inverted color='blue'>Login</Button>
+            </React.fragment>
+        
+    
     }
-
-
 }
 
 export default LoginForm;
