0eef3e9117ab076c9685ab41e90805208c651e43
Mikaiyl <mikaiyl.davis@icloud.com>
2018-08-30 09:29:14 -0400

    post, messagelist work

--- src/reducers/index.jsx
+++ src/reducers/index.jsx
@@ -1,6 +1,6 @@
-import { LOGIN_USER, LOG_ERROR, REGISTER_USER, GET_MESSAGES, POST_MESSAGE, LOGOUT_USER, GET_USER } from "../actions";
+import { LOGIN_USER, LOG_ERROR, REGISTER_USER, GET_MESSAGES, POST_MESSAGE, LOGOUT_USER, GET_USER, GET_USERS_LIST } from "../actions";
 
-const initialState = { session: {}, user: {}, messages: [] }
+const initialState = { session: {}, user: {}, messages: [], userList: [] }
 
 export default (state = initialState, action) => {
     switch (action.type) {
@@ -25,12 +25,12 @@ export default (state = initialState, action) => {
         case GET_MESSAGES:
             return {
                 ...state,
-                messages: action.messages
+                messages:  [ ...action.messages ]
             }
         case POST_MESSAGE:
             return {
                 ...state,
-                messages: [ state.messages, { text: action.message, likes: [], id: undefined, createdAt: Date() }  ]
+                messages: [ ...state.messages, { text: action.message, likes: [], id: undefined, createdAt: Date() }  ]
             }
         case LOGOUT_USER:
             return {
@@ -41,6 +41,11 @@ export default (state = initialState, action) => {
             return {
                 ...state,
                 user: action.user
+            }
+        case GET_USERS_LIST:
+            return {
+                ...state,
+                userList: action.userList.map( user => ( { id: user.id, displayName: user.displayName } ) )
             }
         case LOG_ERROR:
           return {
