2d3d3ff4703e1e56b002170f4274ef7de7533c2e
Mikaiyl <mikaiyl.davis@icloud.com>
2018-09-05 17:29:19 -0400

    Delete Button

--- src/components/RegistrationForm.jsx
+++ src/components/RegistrationForm.jsx
@@ -40,8 +40,7 @@ class RegistrationForm extends Component {
                 <Input  placeholder='Username' onChange={this.updateUsername}/>
                 <Input  placeholder='Display Name' onChange={this.updateDisplay}/>
                 <Input  placeholder='Password' type='password' onChange={this.updatePassword}/>
-              
-          <Button id="regform" color='#0098d3' type='submit' onClick={ () => this.props.registerUser(this.state.username, this.state.displayName, this.state.password) }>submit</Button>
+                <Button id="regform" color='teal' type='submit' onClick={ () => this.props.registerUser(this.state.username, this.state.displayName, this.state.password) }>submit</Button>
               </Grid.Column>
               <Grid.Column></Grid.Column>
             </Grid>
